1972278bd264b3f9f85fdafc693a2f5f60e02d3d
songchao <songchao@mr-gut.cn>
2024-02-18 14:38:52 +0800

    pdf预览

--- serve.js
+++ serve.js
@@ -1,7 +1,34 @@
 const express = require("express");
+const request = require("request");
 const multer = require("multer");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const crypto = require("crypto");
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static("./public/"));
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://www.storkapp.me",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api": "",
+    },
+  })
+);
+
+app.get("/proxy", (req, res) => {
+  const url = req.query.url; // 获取传入的URL参数
+  if (!url || !/^https?:\/\/.+/.test(url)) {
+    return res.status(400).send("Invalid URL!");
+  }
+  // 向目标地址发送GET请求，并将返回结果写入response流中
+  request({ uri: url }).pipe(res);
+});
 
 //实例化multer，传递的参数对象，dest表示上传文件的存储路径
 const upload = multer({
@@ -17,11 +44,6 @@ const upload = multer({
   }),
 });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use(express.static("./public/"));
-
 app.post("/upload", upload.single("file"), (req, res) => {
   try {
     res.json({
@@ -29,6 +51,19 @@ app.post("/upload", upload.single("file"), (req, res) => {
       msg: "上传成功",
       data: `/upload/${req.file.filename}`,
     });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/uploadPdf", (req, res) => {
+  try {
+    console.log(req.body);
+    // res.json({
+    //   code: 200,
+    //   msg: "上传成功",
+    //   data: `/upload/${req.file.filename}`,
+    // });
   } catch (error) {
     console.log(error);
   }
